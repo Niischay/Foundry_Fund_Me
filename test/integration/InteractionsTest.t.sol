2e0907ab703867cb390097d07975b66fc071cb14
Nischay Chhetri <nischaychhetri@gmail.com>
2024-03-16 23:31:54 +0530

    our first commit!

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+
+contract InteractionsTest is Test{
+    FundMe public fundMe;
+    HelperConfig public helperConfig;
+
+    address constant USER = address(1);
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
+    }
+}
